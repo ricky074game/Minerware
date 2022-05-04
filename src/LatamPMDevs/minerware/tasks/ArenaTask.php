1bdc632b0cecd1c9eb3b5b7b0538fc9bd9d33c4e
ricky074game <67124402+ricky074game@users.noreply.github.com>
2022-05-03 20:32:26 -0400

    Update ArenaTask.php

--- src/LatamPMDevs/minerware/tasks/ArenaTask.php
+++ src/LatamPMDevs/minerware/tasks/ArenaTask.php
@@ -97,11 +97,11 @@ final class ArenaTask extends Task {
 				}
 				if ($arena->inbetweentime === 10) {
 					foreach ($players as $player) {
-						$player->sendTitle("§6MinerWare", "§5By LatamPMDevs", 10, 10, 10);
+						$player->sendTitle("§6MinerWare", "§5By Blossom", 10, 10, 10);
 					}
 				} elseif ($arena->inbetweentime === 6) {
 					foreach ($players as $player) {
-						$player->sendTitle("§1§2", "§5Win the most microgames", 10, 10, 10);
+						$player->sendTitle("§1§2", "§5Win the most microgames!", 10, 10, 10);
 					}
 				}
 				if ($arena->inbetweentime <= 3 && $arena->inbetweentime >= 1) {
